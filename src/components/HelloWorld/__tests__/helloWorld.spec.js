8949410240ab1f930e6e05377c4269af75718243
Marco Cesi <sq@iamsquare.it>
2019-11-01 18:37:43 +0100

    Updated example tests

--- src/components/HelloWorld/__tests__/helloWorld.spec.js
+++ src/components/HelloWorld/__tests__/helloWorld.spec.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { snapshotify } from 'TestUtils';
+import { mount } from 'enzyme';
 
 import HelloWorld from '../helloWorld';
 
 describe('HelloWorld', () => {
   it('Exists', () => {
-    const wrapper = shallow(<HelloWorld />);
+    const wrapper = mount(<HelloWorld />);
     expect(wrapper.exists()).toEqual(true);
   });
 
   it('Should render an <h1> element', () => {
-    const wrapper = shallow(<HelloWorld />);
+    const wrapper = mount(<HelloWorld />);
     expect(wrapper.find('h1').exists()).toEqual(true);
   });
 
   it('Should render "Hello, World!"', () => {
-    const wrapper = shallow(<HelloWorld />);
+    const wrapper = mount(<HelloWorld />);
     expect(wrapper.find('h1').text()).toEqual('Hello, World!');
   });
 
   it('Should match snapshot', () => {
-    const wrapper = shallow(<HelloWorld />);
-    expect(snapshotify(wrapper)).toMatchSnapshot();
+    const wrapper = mount(<HelloWorld />);
+    expect(wrapper).toMatchSnapshot();
   });
 });
